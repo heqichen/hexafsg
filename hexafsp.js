ea999b8d0980dc6988e8592c49730ef500a36cc3
HE Qichen <heqichen@gmail.com>
2015-01-21 23:16:06 +0800

    point can rotate on axis-x now

--- hexafsp.js
+++ hexafsp.js
@@ -11,3 +11,8 @@ console.log(p.toString());
 console.log(p.translate(1, 2, 3).toString());
 
 console.log(p.translate(p).toString());
+
+var p2 = new Point3D(1, 2, 3);
+p2.rotate(90, 0, 0).rotate(45, 0, 0);
+
+console.log(p2.toString());
--- point-3d.js
+++ point-3d.js
@@ -1,5 +1,7 @@
 "use strict"
 
+var PI = 3.1415926535897932384626433832795;
+
 var Point3D = function(sx, sy, sz) {
 
 var self = this;
@@ -33,12 +35,38 @@ this.toString = function() {
 this.translate = function(p1, p2, p3) {
 	if (p2 === undefined && p3 === undefined) {
 		translateDimension(p1.x, p1.y, p1.z);
-		return self;
 	} else {
 		translateDimension(p1, p2, p3);
-		return self;
 	}
+
+	return self;
+};
+
+var rotateX = function(angle) {
+	angle = angle * PI / 180.0;
+	var temp = self.y;
+	self.y = self.y * Math.cos(angle) - self.z*Math.sin(angle);
+	self.z = temp * Math.sin(angle) + self.z * Math.cos(angle);
 }
+
+this.rotate = function (p1, p2, p3) {
+	var angleX = 0;
+	var angleY = 0;
+	var angleZ = 0;
+	if (p2===undefined && p3===undefined) {
+		angleX = p1.x;
+		angleY = p1.y;
+		angleZ = p1.z
+	} else {
+		angleX = p1;
+		angleY = p2;
+		angleZ = p3;
+	}
+
+	rotateX(angleX);
+
+	return self;
+};
 
 
 };
